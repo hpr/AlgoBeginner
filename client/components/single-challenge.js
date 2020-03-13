6c57d31f164aaa7eab4a1d664f5dcfb227facffa
Harry <habs@sdf.org>
2020-03-13 17:43:00 -0400

    add editor

--- client/components/single-challenge.js
+++ client/components/single-challenge.js
@@ -3,12 +3,44 @@ import {connect} from 'react-redux';
 import {getChallenge} from '../store';
 
 class SingleChallenge extends React.Component {
-  componentDidMount() {
-    this.props.getChallenge(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: ''
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+  async componentDidMount() {
+    await this.props.getChallenge(this.props.match.params.id);
+    this.setState({
+      code: `function ${this.props.challenge.functionName}(input) {\n\n};`
+    });
+  }
+  onChange(evt) {
+    this.setState({
+      [evt.target.name]: [evt.target.value]
+    });
   }
   render() {
     const {challenge} = this.props;
-    return <div>{challenge.name}</div>;
+    return (
+      <div>
+        <h1>{challenge.name}</h1>
+        <p>{challenge.description}</p>
+        <form>
+          <div className="editor">
+            <textarea
+              name="code"
+              rows={25}
+              cols={80}
+              value={this.state.code}
+              onChange={this.onChange}
+            />
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
